6d17c6f06ae0ca82f491bb69c28c760e46493e2a
Kaden Adlington <kadenadlington@mmc.school.nz>
2020-06-23 15:07:36 +1200

    Start of DodgeDemo, planets displaying but not multiple only a single planet.

---
+++ DodgeDemo/DodgeDemo/FrmDodge.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DodgeDemo
+{
+    public partial class FrmDodge : Form
+    {
+        Graphics g; //declare a graphics object called g
+        Planet[] planet = new Planet[7];
+
+        public FrmDodge()
+        {
+            InitializeComponent();
+            for (int i = 0; i < 7; i++)
+            {
+                planet[i] = new Planet();
+            }
+
+        }
+
+        private void PnlGame_Paint(object sender, PaintEventArgs e)
+        {
+            //get the graphics used to paint on the panel control
+            g = e.Graphics;
+            for (int i = 0; i < 7; i++)
+            {
+                //call the Planet class's drawPlanet method to draw the images
+                planet[i].DrawPlanet(g);
+            }
+
+
+        }
+    }
+}
---
+++ DodgeDemo/DodgeDemo/Planet.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Threading.Tasks;
+
+namespace DodgeDemo
+{
+    class Planet
+    {
+        // declare fields to use in the class
+        public int x, y, width, height;//variables for the rectangle
+        public Image planetImage;//variable for the planet's image
+
+        public Rectangle planetRec;//variable for a rectangle to place our image in
+        public int score;
+        //Create a constructor (initialises the values of the fields)
+        public Planet(int spacing)
+        {
+            x = spacing;
+            y = 10;
+            width = 20;
+            height = 20;
+            //planetImage contains the plane1.png image
+            planetImage = Properties.Resources.planet1;
+            planetRec = new Rectangle(x, y, width, height);
+        }
+        // Methods for the Planet class
+        public void DrawPlanet(Graphics g)
+        {
+            planetRec = new Rectangle(x, y, width, height);
+            g.DrawImage(planetImage, planetRec);
+        }
+
+
+    }
+}
